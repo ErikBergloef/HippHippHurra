f1460be2cd2e2607ac63f2d0f8011ac3697b6e5e
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-05-10 08:49:13 +0200

    Add files via upload

---
+++ HippHippHurra/HippHippHurra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace HippHippHurra
+{
+    class Hurra
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            string hurra = "Hipp hipp hurra!";
+
+            for (int loop = 0; loop < 20; loop++)
+            {
+                Console.WriteLine(hurra);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
